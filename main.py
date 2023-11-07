cbb80a0cf6666e7689e72c35714739ca6b423d20
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2023-11-07 21:11:12 +0100

    0.3
    - dotenv suppport (won't work in binary)
    - cleaned up the code a bit

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import pygetwindow as gw
 import pyautogui
 import requests
 from datetime import datetime
-from os import system
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-system("title " + "Server Uptime Announcer")
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+startup_delay = os.environ.get("time")
+webhook_url = os.environ.get("webhook_url")
+gameserver_name = os.environ.get("gameserver_name")
+
+os.system("title " + "Server Uptime Announcer")
 print("made with love by apfelteesaft\nmy github: https://github.com/apfelteesaft \n")
 
 def is_window_open(window_name):
@@ -17,11 +25,6 @@ def wait_for_window_to_close(window_name):
         time.sleep(10)
 
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-#adjust these settings to your infos
-startup_delay = 65 # the ammount of time in seconds it takes for your gameserver to be joinable (from when the window opens)
-webhook_url = "your_webhook_url_here" # the webhook url for your discord webhook
-gameserver_name = "Project Reboot" # leave this to "Project Reboot" if you are using the Reboot GS, otherwise the name of the gameserver is the window title! (for example, this is a window title: https://media.discordapp.net/attachments/1154892615901270188/1164148536691142727/image.png?ex=654228cf&is=652fb3cf&hm=87f558780ad41b14f83599e170ce93b9315db9b01295d54ec463a247b142b1a7&=&width=968&height=30)
 
 data = {
     "embeds": [
@@ -35,6 +38,7 @@ data = {
         }
     ]
 }
+
 if __name__ == "__main__":
     target_window_name = gameserver_name
 
@@ -47,4 +51,5 @@ if __name__ == "__main__":
             wait_for_window_to_close(target_window_name) # when gameserver closes, it enters a loop where it waits for the gameserver to start again
         else:
             print("Waiting for the process...")
+            print(gameserver_name)
             time.sleep(10)
